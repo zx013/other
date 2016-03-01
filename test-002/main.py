2ac1ab498d1a57c872979bc4feeff60ae092eafc
zx013 <1453145267@qq.com>
2016-03-01 17:08:45 +0800

    step

--- test-002/main.py
+++ test-002/main.py
@@ -5,13 +5,39 @@ kivy.require('1.9.0')
 
 from kivy.app import App
 from kivy.uix.label import Label
-from alipay import Alipay
+
+
+import jnius_config
+jnius_config.add_classpath('.', 'paydemoactivity.jar')
+from jnius import autoclass
+
+def test():
+	Stack = autoclass('java.util.Stack')
+	stack = Stack()
+	stack.push('hello')
+	stack.push('world')
+	stack.pop()
+
+def pay(*args):
+	PayDemoActivity = autoclass('com.alipay.sdk.pay.demo.PayDemoActivity')
+	s = str(PayDemoActivity) + '\n'
+	try:
+		demo = PayDemoActivity()
+	except Exception, ex:
+		s += str(ex) + '\n'
+	try:
+		demo.pay()
+	except Exception, ex:
+		s += str(ex) + '\n'
+	return s
+
 
 class mainApp(App):
 	def build(self):
-		alipay = Alipay()
-		s = alipay.run()
+		test()
+		s = pay()
 		label = Label(text=s)
+		
 		return label
 
 if __name__ == '__main__':
