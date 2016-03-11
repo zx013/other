0b69ee117ec1312300eefcd68432cdc14d0284e0
zx013 <1453145267@qq.com>
2016-03-11 15:58:42 +0800

    key sample

--- test-002/pay.py
+++ test-002/pay.py
@@ -162,6 +162,7 @@ class WxPay:
 	NOTIFY_URL = 'http://notify.msp.hk/notify.htm'
 	ACCESS_TOKEN = ''
 	APP_ID = 'wxb4ba3c02aa476ea1'
+	APP_SECRET = 'db426a9829e4b49a0dcac7b4162da6b6'
 	APP_KEY = 'L8LrMqqeGRxST5reouB0K66CaYAWpqhAVsq7ggKkxHCOastWksvuX1uvmvQclxaHoYd3ElNBrNO2DHnnzgfVG9Qs473M3DTOZug5er46FhuGofumV8H2FVR9qkjSlC5K'
 	PARTNER_ID = '1900000109'
 	PARTNER_KEY = '8934e7d15453e97507ef794cf7b0519d'
@@ -181,7 +182,7 @@ class WxPay:
 
 	@repeat_func(3)
 	def getAccessToken(self):
-		url = 'https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=%s&secret=APPSECRET' % self.APP_ID
+		url = 'https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=%s&secret=%s' % (self.APP_ID, self.APP_SECRET)
 		result = self.getUrl(url)
 		if not result:
 			return
