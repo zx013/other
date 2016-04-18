7254068abca74c0382839e54ffa0575be163c2d3
zx013 <1453145267@qq.com>
2016-04-18 15:30:08 +0800

    route

--- test-003/core/battle/buff.py
+++ test-003/core/battle/buff.py
@@ -8,10 +8,10 @@ class Buff(object):
 class BuffPool(object):
 	def __init__(self, **kwargs):
 		self.buffpool = []
-	
+
 	def insert(self, buff):
 		self.buffpool.append(buff)
-	
+
 	def delete(self, buff):
 		pass
 
